bc88e81b33e354d08752298f455731b05032873c
davidvargas <vargaspu21@up.edu>
2020-10-01 17:05:32 -0700

    implements part 2 of flixster

---
+++ flixster/MovieGridCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCollectionViewCell.swift
+//  flixster
+//
+//  Created by David Vargas on 10/1/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCollectionViewCell: UICollectionViewCell {
+    
+}
